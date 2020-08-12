20d2ebd9949872495ee85d9140037c232b8e5ac8
minimuscle <joshthiele@live.com.au>
2020-08-12 16:00:50 +1000

    eslint

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 
+//Boostrap Imports - Design work
+import { Navbar, Nav, Form, Button, FormControl, NavDropdown } from 'react-bootstrap';
+
 class App extends Component {
-  render(){
+  render() {
     return (
-    <div><h1>Hello</h1></div>
+      <div className="container">
+        <Navbar>
+          <Nav className=''>
+            
+          </Nav>
+        </Navbar>
+      </div>
     )
   }
 }
